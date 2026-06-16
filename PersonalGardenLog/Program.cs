7144a1e77f9635e1e77820d6bf0de23e30347118
Ririku Shirakawa <ririkuchan@gmail.com>
2026-06-16 17:51:54 +0900

    Successfully configure MudBlazor layout settings and target .NET 8.0

--- PersonalGardenLog/Program.cs
+++ PersonalGardenLog/Program.cs
@@ -1,11 +1,18 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services; // 👈 より確実な名前空間を指定します
+using System;
+using System.Net.Http;
 using PersonalGardenLog;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// ⭐ MudBlazor のサービスを登録
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
